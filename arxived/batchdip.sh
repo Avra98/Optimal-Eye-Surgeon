03e486b2d3107926f08af5f4fd702605b86a77b7
Avra98 <avrajitghosh@gmail.com>
2024-05-26 20:01:27 -0400

    Remove unnecessary files and directories, update .gitignore

--- arxived/batchdip.sh
+++
@@ -1,8 +0,0 @@
-python -u deep_hess_batch.py --max_steps=8000 --optim="SGD" --sigma=0.1 --device_id=0 &
-python -u -deep_hess_batch.py -max_steps=8000 --optim="SGD" --sigma=0.05 --device_id=2 &
-python -u deep_hess_batch.py --max_steps=8000 --optim="SAM" --sigma=0.1 --device_id=4 &
-python -u deep_hess_batch.py --max_steps=8000 --optim="SAM" --sigma=0.05 --device_id=6
-
-
-
-
