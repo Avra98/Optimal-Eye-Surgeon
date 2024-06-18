79274d8c958417b6be44ec48e22bda878769d7e1
Kenneth Sun <kennett.sun@gmail.com>
2024-06-17 21:30:39 -0400

    clean baseline_pai.py

--- baseline_pai.py
+++ baseline_pai.py
@@ -1,34 +1,25 @@
 from __future__ import print_function
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 import os
-import glob
-import sys
-from scipy.ndimage import gaussian_filter
 import warnings
-warnings.filterwarnings("ignore")
 import numpy as np
+import torch
+import torch.optim
+import argparse
+from utils.inpainting_utils import *
 from utils.denoising_utils import *
 from models import *
 from utils.quant import *
 from utils.imp import *
-from models.cnn import cnn
-import torch
-import torch.optim
-import time
-from utils.inpainting_utils import *
-import pickle as cPickle
+
+warnings.filterwarnings("ignore")
+
+dtype = torch.cuda.FloatTensor
 torch.backends.cudnn.enabled = True
 torch.backends.cudnn.benchmark = True
-dtype = torch.cuda.FloatTensor
-from torch.nn.utils import parameters_to_vector, vector_to_parameters
 
-import argparse
-
-
-def main(lr: float, max_steps: int, optim: str, reg: float = 0.0, sigma: float = 0.2,
-         num_layers: int = 4, show_every: int = 1000, device_id: int = 0, beta: float = 0.0,
-         image_name: str = 'baboon', weight_decay: float = 0.0, sparse: float = 0.5, prune_type: str = "rand_global"):
+def main(max_steps: int, sigma: float = 0.2, num_layers: int = 4, show_every: int = 1000, device_id: int = 0, 
+         image_name: str = 'baboon', sparse: float = 0.5, prune_type: str = "rand_global"):
 
     torch.cuda.set_device(device_id)
     torch.cuda.current_device()
@@ -41,7 +32,6 @@ def main(lr: float, max_steps: int, optim: str, reg: float = 0.0, sigma: float =
     input_depth = 32
     output_depth = 3
 
-    mse = torch.nn.MSELoss().type(dtype)
     INPUT = "noise"
 
     net_input = get_noise(input_depth, INPUT, img_np.shape[1:]).type(dtype)
@@ -82,7 +72,8 @@ def main(lr: float, max_steps: int, optim: str, reg: float = 0.0, sigma: float =
     elif prune_type == "synflow_local":
         mask = synflow_prune_local(net, mask, net_input, sparse)
 
-    psnr, out = train_sparse(net, net_input, mask, img_np, img_noisy_np, max_step=max_steps, show_every=show_every, device=device_id)
+    psnr, out = train_sparse(net, net_input, mask, img_np, img_noisy_np,
+                             max_step=max_steps, show_every=show_every, device=device_id)
 
     outdir = f'images/{image_name}/pai/{prune_type}_{sparse}'
     print(f"Output directory: {outdir}")
@@ -129,7 +120,6 @@ if __name__ == "__main__":
 
     parser.add_argument("--lr", type=float, default=1e-2, help="the learning rate")
     parser.add_argument("--max_steps", type=int, default=40000, help="the maximum number of gradient steps to train for")
-    parser.add_argument("--optim", type=str, default="ADAM", help="which optimizer")
     parser.add_argument("--reg", type=float, default=0.05, help="if regularization strength of igr")
     parser.add_argument("--sigma", type=float, default=0.1, help="noise-level")
     parser.add_argument("--num_layers", type=int, default=6, help="number of layers")
@@ -143,7 +133,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    main(lr=args.lr, max_steps=args.max_steps,
-         optim=args.optim, reg=args.reg, sigma=args.sigma, num_layers=args.num_layers,
-         show_every=args.show_every, beta=args.beta, device_id=args.device_id, image_name=args.image_name,
-         weight_decay=args.decay, sparse=args.sparse, prune_type=args.prune_type)
+    main(max_steps=args.max_steps,
+         sigma=args.sigma, num_layers=args.num_layers,
+         show_every=args.show_every, device_id=args.device_id, image_name=args.image_name,
+         sparse=args.sparse, prune_type=args.prune_type)
